8cd92e8e8866f8595464890de3e79fd0b8052afb
Shoraku <alamgaru@hotmail.de>
2024-06-15 15:13:34 +0200

    final

--- ros2_test/build/my_package2/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/shorif/Schreibtisch/Masterarbeit/ros2_test/install/my_package2'
--- ros2_test/install/interfaces/share/interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(interfaces_IDL_FILES "srv/MakeSquare.idl;action/MakeSquare.idl")
-set(interfaces_INTERFACE_FILES "srv/MakeSquare.srv;srv/MakeSquare_Request.msg;srv/MakeSquare_Response.msg;action/MakeSquare.action")
